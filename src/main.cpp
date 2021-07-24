bdeabdd8c4122bfad8f7f9a24be36ceefd3d9670
Denver <denverclarke@live.com>
2021-07-23 21:05:32 -0600

    using local branch and added dual core test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,53 @@
 #include <Arduino.h>
-#define FASTLED_ALLOW_INTERRUPTS 0
+static TaskHandle_t FastLEDshowTaskHandle = 0;
+static TaskHandle_t userTaskHandle = 0;
 #include <dumbconfig.h>
 //#include <dumbbt.h>
 #include <BluetoothSerial.h>
 BluetoothSerial Bluetooth;
 
+void FastLEDshowESP32()
+{
+    if (userTaskHandle == 0) {
+        const TickType_t xMaxBlockTime = pdMS_TO_TICKS( 200 );
+        // -- Store the handle of the current task, so that the show task can
+        //    notify it when it's done
+       // noInterrupts();
+        userTaskHandle = xTaskGetCurrentTaskHandle();
+        
+        // -- Trigger the show task
+        xTaskNotifyGive(FastLEDshowTaskHandle);
+
+        // -- Wait to be notified that it's done
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+        //delay(100);
+        //interrupts();
+        userTaskHandle = 0;
+    }
+}
+
+void FastLEDshowTask(void *pvParameters)
+{
+    const TickType_t xMaxBlockTime = pdMS_TO_TICKS( 500 );
+    // -- Run forever...
+    for(;;) {
+        // -- Wait for the trigger
+        ulTaskNotifyTake(pdTRUE,portMAX_DELAY);
+        
+     // time3=ESP.getCycleCount();
+    
+         //controller.showPixels(); /
+     FastLED.show();
+       // Serial.printf("FPS:%f\n",(float)(240000000/(ESP.getCycleCount()-time3)));
+
+    
+        xTaskNotifyGive(userTaskHandle);
+    }
+}
+
 void basic(){
   static byte huep;
   EVERY_N_MILLIS(4){
-    FastLED.clear();
     //for(int i = 0; i < NUM_LEDS; i++){
     //  leds[i] = CHSV(hue + i*5, 255, 255);
     //}
@@ -25,7 +64,7 @@ void basic(){
     
     huep++;
     //long lastHandle = __clock_cycles();
-    FastLED.show();
+    FastLEDshowESP32();
     //long lasthandle2=__clock_cycles();
     //Serial.printf("FPS: %f \n", (float) 240000000L/(lasthandle2 - lastHandle));
     EVERY_N_MILLIS(500){
@@ -38,11 +77,13 @@ void basic(){
 }
 
 void setup() {
-    Bluetooth.begin("blooth");
-    Serial.begin(115200);
-    //FastLED.setDither(0);
-    FastLED.addLeds<VIRTUAL_DRIVER,Pins,CLOCK_PIN, LATCH_PIN>(leds,NUM_LEDS_PER_STRIP);
-    FastLED.setBrightness(25);
+  Bluetooth.begin("blooth");
+  Serial.begin(115200);
+  //FastLED.setDither(0);
+  FastLED.addLeds<VIRTUAL_DRIVER,Pins,CLOCK_PIN, LATCH_PIN>(leds,NUM_LEDS_PER_STRIP);
+  FastLED.setBrightness(25);
+  xTaskCreatePinnedToCore(FastLEDshowTask, "FastLEDshowTask", 2000, NULL,2, &FastLEDshowTaskHandle, 0);
+// now use FastLEDshowESP32() instead of fastled.show()
 }
 
 void loop() {
